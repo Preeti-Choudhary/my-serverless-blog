7a8382903cc4060750e7108eed37054ea88adcc1
Preeti-Choudhary <c.preeti3@gmail.com>
2019-08-14 12:42:07 +0000

    first commit

--- content/blog/my-second-post/index.md
+++ content/blog/my-second-post/index.md
@@ -4,5 +4,6 @@ date: "2019-08-14T23:46:37.121Z"
 ---
 
 Preeti's first blog! I love blogging so much already.
+Using Amplify and Gatsby
 
 Loads to learn
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Gatsby Preeti Blog`,
+    author: `Preeti Choudhary`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `pchoudhary`,
     },
   },
   plugins: [
